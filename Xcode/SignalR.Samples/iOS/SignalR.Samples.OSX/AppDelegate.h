0cb934277cfa6dab0f438977b956be65b3774ee5
Alex Billingsley <abillingsley@dyknow.com>
2012-01-17 18:51:44 -0500

    #10 initial mac sample app

---
+++ Xcode/SignalR.Samples/iOS/SignalR.Samples.OSX/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SignalR.Samples.OSX
+//
+//  Created by Alex Billingsley on 1/17/12.
+//  Copyright (c) 2012 DyKnow LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ Xcode/SignalR.Samples/iOS/SignalR.Samples.OSX/AppDelegate.m
@@ -0,0 +1,20 @@
+//
+//  AppDelegate.m
+//  SignalR.Samples.OSX
+//
+//  Created by Alex Billingsley on 1/17/12.
+//  Copyright (c) 2012 DyKnow LLC. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
