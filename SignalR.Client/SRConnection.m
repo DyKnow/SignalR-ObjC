22480c11446ee28ecae2f373997706803ff69ad4
Alex Billingsley <abillingsley@dyknow.com>
2012-09-11 12:40:43 -0400

    bump version

--- SignalR.Client/SRConnection.m
+++ SignalR.Client/SRConnection.m
@@ -347,7 +347,7 @@ void (^prepareRequest)(id);
 {
     if(_assemblyVersion == nil)
     {
-        _assemblyVersion = [[SRVersion alloc] initWithMajor:0 minor:5 build:3 revision:0];
+        _assemblyVersion = [[SRVersion alloc] initWithMajor:0 minor:5 build:3 revision:1];
     }
    
 #if TARGET_OS_IPHONE || TARGET_IPHONE_SIMULATOR
