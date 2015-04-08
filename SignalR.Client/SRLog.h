05aa6ca235d7df2c803a4637924b4dcf61292874
Shams Ahmed <shams-ahmed@users.noreply.github.com>
2015-04-08 15:54:30 +0100

    by default turn off logging
    at the moment, the project logs out everything to turn it off you would have to modify the existing code which is always the best practice if in a dependency

--- SignalR.Client/SRLog.h
+++ SignalR.Client/SRLog.h
@@ -45,7 +45,7 @@
 #define LOG_WEBSOCKETS          (ddLogLevel & LOG_LEVEL_WEBSOCKETS  )
 #define LOG_AUTOTRANSPORT       (ddLogLevel & LOG_FLAG_AUTOTRANSPORT )
 
-static int ddLogLevel = LOG_LEVEL_AUTOTRANSPORT;
+static int ddLogLevel = LOG_LEVEL_OFF;
 
 #define COCOA_LUMBER_JACK 0
 #if COCOA_LUMBER_JACK
